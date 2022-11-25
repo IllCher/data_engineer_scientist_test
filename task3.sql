fc60a74a499a22700c3c1b2437dfc0a277814988
IllCher <50427226+IllCher@users.noreply.github.com>
2022-11-25 18:16:58 +0300

    Create task3.sql

---
+++ task3.sql
@@ -0,0 +1,10 @@
+create table task3(
+	item_id number(21,0), item_name char(150), 
+	item_price number(12, 2), created_dttm timestamp
+);
+insert into task3 values ('7072110988', 32722, '2021-08-18 13:15');
+insert into task3 values ('7072110988', 32722, '2021-08-18 14:00');
+insert into task3 values ('7072110988', 21534, '2021-08-18 14:15');
+insert into task3 values ('7072110988', 32722, '2021-08-19 09:00');
+insert into task3 values ('7071107101', 12533, '2021-08-19 09:15');
+insert into task3 values ('7071107101', 32722, '2021-08-19 09:27');
