cbbe3b1bf7898cc62a72b25554c26283ecb76efd
IllCher <50427226+IllCher@users.noreply.github.com>
2022-11-25 17:59:54 +0300

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -1,4 +1,5 @@
-/*create table task2(
+/*
+create table task2(
 	abonent char(10), region_id integer, dttm timestamp
 );
 insert into task2 values ('7072110988', 32722, '2021-08-18 13:15');
@@ -12,7 +13,22 @@ insert into task2 values ('7071107101', 32722, '2021-08-19 09:27');
 select *
 from (select abonent, region_id, max(dttm) as dttm from task2
 group by abonent, region_id
-) as foo
+order by dttm
+) as t3
+
+except
+
+select * 
+from (select abonent, region_id, max(dttm) as dttm from task2
+group by abonent, region_id
+order by dttm
+) t1
+where t1.dttm = (select min(t2.dttm) from 
+(select abonent, region_id, min(dttm) as dttm from task2
+group by abonent, region_id
+order by dttm
+) t2 where t2.abonent = t1.abonent)
+order by dttm
 
 
 
