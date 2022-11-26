d651e09535a730654cc9efb45da47b8cc1ef1ebc
IllCher <ilya.chernenko.2012@gmail.com>
2022-11-26 21:15:03 +0300

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -1,1 +1,11 @@
-select * from test order by age limit 3;
+drop table if exists test;
+
+create table test(id integer, name char(10), age integer)
+
+insert into test values (1, 'Вася', 23);
+insert into test values (2, 'Петя', 40);
+insert into test values (3, 'Маша', 19);
+insert into test values (4, 'Марина', 23);
+insert into test values (5, 'Сергей', 34);
+
+select name, age from test order by age limit 3;
