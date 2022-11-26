4782d85dad44d9456f9e049ac31a080f8dd39e43
IllCher <ilya.chernenko.2012@gmail.com>
2022-11-26 22:52:47 +0300

    quality changes

--- task1.sql
+++ task1.sql
@@ -1,6 +1,6 @@
 drop table if exists test;
 
-create table test(id integer, name char(10), age integer)
+create table test(id integer, name char(10), age integer);
 
 insert into test values (1, 'Вася', 23);
 insert into test values (2, 'Петя', 40);
@@ -9,3 +9,12 @@ insert into test values (4, 'Марина', 23);
 insert into test values (5, 'Сергей', 34);
 
 select name, age from test order by age limit 3;
+
+
+select name, age
+from (
+	select name, age,
+		dense_rank() over (order by age) as rank
+	from test
+) tmp
+where rank <= 2;
