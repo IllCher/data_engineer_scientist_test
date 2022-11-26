a5b968fb1083aaaa7e28334bbf59f8ffb35ee395
IllCher <ilya.chernenko.2012@gmail.com>
2022-11-26 13:49:23 +0300

    added task3 and test

--- gen_test_task3.py
+++ gen_test_task3.py
@@ -4,8 +4,11 @@ import datetime
 
 x = random.random()
 # Random float number
-for i in range(10):
+for i in range(3):
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(10))
-    random_time = datetime.datetime.now() + datetime.timedelta(days=int(round(random.uniform(1, 5), 0)))
-    print(int(round(random.uniform(10, 1000), 0)), result_str, round(random.uniform(10.25, 1000.45), 2), random_time)
+    random_time = datetime.datetime.now().replace(microsecond=0) + datetime.timedelta(days=int(round(random.uniform(3, 10), 0)))
+    cur_item_id = int(round(random.uniform(10, 1000), 0)) 
+    print('insert into task3 values','(' + str(cur_item_id) + ",", "'" + str(result_str) + "'" + ",", str(round(random.uniform(10.25, 1000.45), 2)) + ",", "'" + str(random_time + datetime.timedelta(days=int(round(random.uniform(5, 10), 0)))) + "');")
+    print('insert into task3 values','(' + str(cur_item_id) + ",", "'" + str(result_str) + "'" + ",", str(round(random.uniform(10.25, 1000.45), 2)) + ",", "'" + str(random_time + datetime.timedelta(days=int(round(random.uniform(5, 10), 0)))) + "');")
+    print('insert into task3 values','(' + str(cur_item_id) + ",", "'" + str(result_str) + "'" + ",", str(round(random.uniform(10.25, 1000.45), 2)) + ",", "'" + str(random_time) + "');")
--- task3.sql
+++ task3.sql
@@ -1,10 +1,32 @@
+/*
 create table task3(
-	item_id number(21,0), item_name char(150), 
-	item_price number(12, 2), created_dttm timestamp
+	item_id numeric(21,0), item_name char(150), 
+	item_price numeric(12, 2), created_dttm timestamp
 );
-insert into task3 values ('7072110988', 32722, '2021-08-18 13:15');
-insert into task3 values ('7072110988', 32722, '2021-08-18 14:00');
-insert into task3 values ('7072110988', 21534, '2021-08-18 14:15');
-insert into task3 values ('7072110988', 32722, '2021-08-19 09:00');
-insert into task3 values ('7071107101', 12533, '2021-08-19 09:15');
-insert into task3 values ('7071107101', 32722, '2021-08-19 09:27');
+insert into task3 values (130, 'lygvvufltx', 622.56, '2022-12-09 13:17:08');
+insert into task3 values (130, 'lygvvufltx', 377.07, '2022-12-07 13:17:08');
+insert into task3 values (130, 'lygvvufltx', 710.48, '2022-11-30 13:17:08');
+insert into task3 values (62, 'duzzrplnih', 897.54, '2022-12-10 13:17:08');
+insert into task3 values (62, 'duzzrplnih', 929.31, '2022-12-09 13:17:08');
+insert into task3 values (62, 'duzzrplnih', 28.72, '2022-12-03 13:17:08');
+insert into task3 values (214, 'nhxzhjtpul', 971.26, '2022-12-06 13:17:08');
+insert into task3 values (214, 'nhxzhjtpul', 924.99, '2022-12-10 13:17:08');
+insert into task3 values (214, 'nhxzhjtpul', 194.71, '2022-11-30 13:17:08');
+*/
+with tmp_table as (
+	select item_id, item_name, item_price, created_dttm::date as valid_from_dt, 
+	(
+		select min(closest_date.created_dttm::date - 1)
+		from task3 as closest_date
+		where closest_date.created_dttm::date > task3.created_dttm::date
+		and  task3.item_id = closest_date.item_id
+	
+	) as valid_to_dt from task3
+)
+select item_id, item_name, item_price, valid_from_dt, coalesce(valid_to_dt, '9999-12-31') as valid_to_dt from tmp_table
+
+
+
+
+
+
